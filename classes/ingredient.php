118a88ff0c94cfe113d59229c98413de3c92c922
Pierre Merlin <merlinp82@gmail.com>
2013-10-25 00:06:14 +0200

    Hello ingredients

---
+++ classes/ingredient.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tests\units;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+include __DIR__ . '/../../classes/helloWorld.php';
+
+use \mageekguy\atoum;
+use \recette;
+
+class ingredient
+{
+  
+}
---
+++ tests/units/ingredient.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace tests\units;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+include __DIR__ . '/../../classes/ingredient.php';
+
+use \mageekguy\atoum;
+use \recette;
+
+class Ingredient extends atoum\test
+{
+    public function testGetName()
+    {
+        $ingredient = new \ingredient();
+
+        
+    }
+}
