6b7ef9ba0a6ac6f56958d4285e54531ef83a2a95
Stéphane PY <py.stephane1@gmail.com>
2013-10-24 10:50:53 +0200

    Remove atoum.phar and use composer binary, some fixes.

--- classes/helloWorld.php
+++ classes/helloWorld.php
@@ -1,5 +1,5 @@
-<?
- 
+<?php
+
 class HelloWorld
 {
     public function getHiAtoum ()
--- tests/units/HelloWorld.php
+++ tests/units/HelloWorld.php
@@ -1,21 +1,21 @@
 <?php
 
-namespace recette\tests\units;
+namespace tests\units;
 
-require_once __DIR__ . '/../../mageekguy.atoum.phar';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
-include '/classes/helloWorld.php';
+include __DIR__ . '/../../classes/helloWorld.php';
 
 use \mageekguy\atoum;
 use \recette;
 
-class helloWorld extends atoum\test
+class HelloWorld extends atoum\test
 {
     public function testSay()
     {
-        $helloWorld = new helloWorld();
+        $helloWorld = new \HelloWorld();
 
-        $this->string($helloWorld->say())->isEqualTo('Hello World!')
+        $this->string($helloWorld->getHiAtoum())->isEqualTo('Hello World!')
         ;
     }
 }
