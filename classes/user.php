727e850c7569f6d4700e905e9a51901d68597430
Nicolas B <nbaubillier@yahoo.fr>
2013-10-24 14:36:41 +0200

    Create user.php

---
+++ classes/user.php
@@ -0,0 +1,41 @@
+<?php
+
+class User{ 
+
+    private $login;
+    private $pass;
+    private $rights;
+    
+    function __construct($login, $pass, $right="user") {
+        $this->login = $login;
+        $this->pass = $pass;
+        $this->right = $right;
+    }
+    public function getLogin() {
+        return $this->login;
+    }
+
+    public function getPass() {
+        return $this->pass;
+    }
+
+    public function getRight() {
+        return $this->right;
+    }
+
+    public function setLogin($login) {
+        $this->login = $login;
+        return $this;
+    }
+
+    public function setPass($pass) {
+        $this->pass = $pass;
+        return $this;
+    }
+
+    public function setRight($right) {
+        $this->right = $right;
+        return $this;
+    }
+}
+?>
