f61231b6cbae149a510bf0686320136066ab0190
Pierre Merlin <merlinp82@gmail.com>
2013-10-25 07:42:33 +0200

    fix ingredient

--- classes/ingredient.php
+++ classes/ingredient.php
@@ -1,14 +1,5 @@
 <?php
 
-namespace tests\units;
-
-require_once __DIR__ . '/../../vendor/autoload.php';
-
-include __DIR__ . '/../../classes/helloWorld.php';
-
-use \mageekguy\atoum;
-use \recette;
-
 class ingredient
 {
   
